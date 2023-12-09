a7dd176896ada38e42374c82e58283dbc2fbc205
Josh Bush <josh@digitalbush.com>
2023-12-09 15:28:31 -0600

    chore: fix broken prettier CHANGELOG formatting

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -2,11 +2,11 @@
 gh_url=$(gh repo view --json url -q ".url")
 prev_tag=$(git describe --tags --abbrev=0)
 new_version=$(npm pkg get version|xargs)
-new_log=$(git log --pretty="* %s ([%h]($gh_url/commit/%H))" $prev_tag..)
+new_log=$(git log --pretty="- %s ([%h]($gh_url/commit/%H))" $prev_tag..)
 
 echo "## $new_version ($(date +"%Y-%m-%d"))
-$new_log
 
+$new_log
 
 $(cat CHANGELOG.md)" > CHANGELOG.md
 
